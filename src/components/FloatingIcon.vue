6d5d7ca49bfdb915571a5c777eec5883ade859fa
Leonardo Overbeck da Silva <overbecksilva.leonardo@gmail.com>
2022-03-21 21:26:36 -0300

    style: finalizing floating button animations

--- src/components/FloatingIcon.vue
+++ src/components/FloatingIcon.vue
@@ -1,6 +1,13 @@
 <template>
-  <a class="floating-icon">
-    <i class="nes-icon coin"></i>
+  <a class="floating-icon"
+     :class="{expanding: isExpanding, retracting: isRetracting, expanded: isExpanded, retracted: isRetracted}"
+     @mouseenter="setExpanding"
+     @mouseleave="setRetracting"
+  >
+    <div class="icon-content-wrapper">
+      <i class="nes-icon coin"></i>
+      <p>info</p>
+    </div>
   </a>
 </template>
 
@@ -9,6 +16,40 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'FloatingIcon',
+  data() {
+    return {
+      isExpanding: false,
+      isRetracting: false,
+      isExpanded: false,
+      isRetracted: true,
+      animationDuration: 250,
+    };
+  },
+  methods: {
+    async setExpanding() {
+      this.isExpanding = true;
+      this.isRetracted = false;
+
+      await this.sleep(this.animationDuration);
+
+      this.isExpanding = false;
+      this.isExpanded = true;
+    },
+
+    async setRetracting() {
+      this.isExpanded = false;
+      this.isRetracting = true;
+
+      await this.sleep(this.animationDuration);
+
+      this.isRetracted = true;
+      this.isRetracting = false;
+
+    },
+    sleep(ms) {
+      return new Promise(r => setTimeout(r, ms));
+    }
+  }
 });
 </script>
 
@@ -18,15 +59,78 @@ export default defineComponent({
 
 $icon-size: 32px;
 $float-anchor-size: 60px;
+$float-anchor-size-expanded: calc(#{$float-anchor-size} * 2.35);
+$animation-length: 250ms;
 
 .floating-icon {
-  position: absolute;
+  position: fixed;
   display: block;
-  width: $float-anchor-size;
   height: $float-anchor-size;
-  border-radius: 100%;
+  border-radius: calc(#{$float-anchor-size} / 2);
   background-color: $background-secondary;
-  text-align: center;
   padding-top: calc((#{$float-anchor-size} - #{$icon-size}) / 2);
+  padding-left: calc((#{$float-anchor-size} - #{$icon-size}) / 2);
+  bottom: 40px;
+  left: 40px;
+  overflow: hidden;
+  z-index: 1;
+}
+
+.icon-content-wrapper {
+  display: flex;
+  margin: 0;
+  padding: 0;
+  height: $float-anchor-size;
+  width: $float-anchor-size;
+}
+
+i {
+  margin: 0;
+}
+
+p {
+  margin-left: calc(#{$icon-size} * 1.37);
+  margin-top: 3px;
+  color: $background-primary;
+}
+
+
+.retracted {
+  width: $float-anchor-size;
+}
+
+.expanded {
+  width: $float-anchor-size-expanded;
+}
+
+@keyframes expand {
+  from {
+    width: $float-anchor-size;
+  }
+
+  to {
+    width: $float-anchor-size-expanded;
+
+  }
+}
+
+.expanding {
+  animation: expand $animation-length ease-in-out forwards;
+
+}
+
+@keyframes retract {
+  from {
+    width: calc(#{$float-anchor-size} * 2.35);
+  }
+
+  to {
+    width: $float-anchor-size;
+  }
+}
+
+.retracting {
+  animation: retract $animation-length ease-in-out forwards;
+
 }
 </style>
