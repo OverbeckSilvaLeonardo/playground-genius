5a0f2d4cf1fe89436fd08505c8b66578c6391850
Leonardo Overbeck da Silva <loverbeck@imply.com>
2023-01-12 17:57:16 -0300

    feature: starting new Pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import "../node_modules/nes.css/css/nes.min.css";
+import { key, pinia, store } from "@/store";
 import "../node_modules/bulma/css/bulma.min.css";
+import "../node_modules/nes.css/css/nes.min.css";
 import "./scss/_variables.scss";
-import { key, store } from "@/store";
 
 createApp(App)
-  .use(store, key)
+  .use(store, key, pinia)
   .mount("#app");
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,55 +1,58 @@
-import {createStore, useStore as vuexUseStore, Store, Commit} from 'vuex';
-import {InjectionKey} from 'vue';
-import IGameMode from '@/services/gamemode.interface';
 import GameModeFactory from '@/services/gamemode.factory';
+import IGameMode from '@/services/gamemode.interface';
 import * as types from '@/store/mutation-types';
+import { createPinia } from 'pinia';
+import { InjectionKey } from 'vue';
+import { Commit, createStore, Store, useStore as vuexUseStore } from 'vuex';
 
 interface GameState {
-    gameMode: number;
-    difficulty: number;
-    isRunning: boolean;
-    service: IGameMode;
+  gameMode: number;
+  difficulty: number;
+  isRunning: boolean;
+  service: IGameMode;
 }
 
 const state = {
-    gameMode: 1,
-    difficulty: 1,
-    isRunning: false,
-    service: GameModeFactory.build(1)
+  gameMode: 1,
+  difficulty: 1,
+  isRunning: false,
+  service: GameModeFactory.build(1),
 };
 
 const mutations = {
-    [types.SET_DIFFICULTY](state: GameState, difficulty: number): void {
-        state.difficulty = difficulty;
-    },
-    [types.SET_GAME_MODE](state: GameState, gameMode: number): void {
-        state.gameMode = gameMode;
-        state.service = GameModeFactory.build(gameMode);
-    },
-    [types.SET_GAME_RUNNING](state: GameState, running: boolean): void {
-        state.isRunning = running;
-    },
+  [types.SET_DIFFICULTY](state: GameState, difficulty: number): void {
+    state.difficulty = difficulty;
+  },
+  [types.SET_GAME_MODE](state: GameState, gameMode: number): void {
+    state.gameMode = gameMode;
+    state.service = GameModeFactory.build(gameMode);
+  },
+  [types.SET_GAME_RUNNING](state: GameState, running: boolean): void {
+    state.isRunning = running;
+  },
 };
 
 const actions = {
-    setDifficulty({commit}: { commit: Commit }, difficulty: number): void {
-        commit(types.SET_DIFFICULTY, difficulty)
-    },
-    setGameMode({commit}: { commit: Commit }, gameMode: number): void {
-        commit(types.SET_GAME_MODE, gameMode)
-    },
-    setGameRunning({commit}: { commit: Commit }, running: boolean): void {
-        commit(types.SET_GAME_RUNNING, running);
-    }
-}
+  setDifficulty({ commit }: { commit: Commit }, difficulty: number): void {
+    commit(types.SET_DIFFICULTY, difficulty);
+  },
+  setGameMode({ commit }: { commit: Commit }, gameMode: number): void {
+    commit(types.SET_GAME_MODE, gameMode);
+  },
+  setGameRunning({ commit }: { commit: Commit }, running: boolean): void {
+    commit(types.SET_GAME_RUNNING, running);
+  },
+};
 export const store = createStore<GameState>({
-    state,
-    actions,
-    mutations
+  state,
+  actions,
+  mutations,
 });
 
 export const key: InjectionKey<Store<GameState>> = Symbol();
 
 export function useStore(): Store<GameState> {
-    return vuexUseStore(key);
+  return vuexUseStore(key);
 }
+
+export const pinia = createPinia();
