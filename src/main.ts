d62b3f3b294cca7214a2d9f48f5849e14c9e8b6c
Leonardo Overbeck da Silva <loverbeck@imply.com>
2023-01-16 08:32:00 -0300

    fix: removing last store import

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import { key, pinia, store } from "@/store";
+import { pinia } from "@/store";
 import "../node_modules/bulma/css/bulma.min.css";
 import "../node_modules/nes.css/css/nes.min.css";
 import "./scss/_variables.scss";
 
 createApp(App)
-  .use(store, key, pinia)
+  .use(pinia)
   .mount("#app");
